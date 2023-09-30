9068a66378f54fa69b656b93520090912e349172
KirkWest <126360978+KirkWest@users.noreply.github.com>
2023-09-30 11:36:40 +1000

    dispatch user created

---
+++ src/ExampleReducer.js
@@ -0,0 +1,17 @@
+
+
+const exampleReducer = (previousState, instructions) => {
+
+  return "blah blah blah, this is a new state"
+}
+
+/*
+
+const reducerFunciton = (previousState, instructions) = > {
+
+
+  // return the NEW state to override the previousState
+  return somethingHere;
+}
+
+*/
